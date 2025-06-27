87185d663ed0d791c0abbf152447f1f2d16ef1fe
Taye-Staats <tayestaats@outlook.com>
2025-06-26 22:24:41 -0700

    Adding C++ to Project
    Linked up Unreal and VSCode, built it in VSCode and then added some variables

---
+++ Obstacle_Assault/Intermediate/Build/Win64/Obstacle_AssaultEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.38.33145
+#include "C:/Users/tayes/OneDrive/Documents/Unreal Projects/Obstacle-Assault/Obstacle_Assault/Intermediate/Build/Win64/Obstacle_AssaultEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
